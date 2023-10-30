cb1d1928343d5624288ea8c090c3586c7f658a85
jgabogado2 <jgabogado2@student.apc.edu.ph>
2023-10-30 21:28:25 +0800

    Files Upload

---
+++ ENTJAVA_Week8/Models/DB/MyDBContext.cs
@@ -0,0 +1,198 @@
+﻿using ENTJAVA_Week8.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Runtime.Intrinsics.X86;
+
+namespace ENTJAVA_Week8.Models.DB
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+        }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<SystemUsers> SystemUsers { get; set; }
+        public virtual DbSet<UserRole> UserRole { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // warning To protect potentially sensitive information in your connection string,
+                // you should move it out of source code.See http://go.microsoft.com/fwlink/?LinkId=723263
+                // for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\\\MyDemoDB;Initial Catalog=DemoDB;Integrated Security=True;Multiple Active Result Sets=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("SYSUserProfile");
+
+                entity.HasKey(e => new { e.ProfileID, e.UserID });
+
+                entity.Property(e => e.ProfileID)
+               .HasColumnName("SYSUserProfileID")
+                  .UseIdentityColumn()
+                .HasColumnType("int");
+
+                entity.Property(e => e.UserID)
+                .HasColumnName("SYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                .HasColumnName("Gender")
+                .HasColumnType("char(1)");
+
+                entity.Property(e => e.CreatedBy)
+                .HasColumnName("RowCreatedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.CreatedDateTime)
+                .HasColumnName("RowCreatedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.ModifiedBy)
+                .HasColumnName("RowModifiedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.ModifiedDateTime)
+                .HasColumnName("RowModifiedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.AccountImage)
+                .HasColumnName("AccountImage")
+                .HasMaxLength(8000)
+                .IsUnicode(false)
+                 .IsRequired(false);
+            });
+
+            modelBuilder.Entity<SystemUsers>(entity =>
+            {
+                entity.ToTable("SYSUser");
+
+                entity.Property(e => e.UserID)
+                .HasColumnName("SYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.LoginName)
+                .HasColumnName("LoginName")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entity.Property(e => e.CreatedBy)
+                .HasColumnName("RowCreatedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.CreatedDateTime)
+                .HasColumnName("RowCreatedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.ModifiedBy)
+                .HasColumnName("RowModifiedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.ModifiedDateTime)
+                .HasColumnName("RowModifiedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            });
+
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.ToTable("SYSUserRole");
+
+                entity.HasKey(e => new { e.RoleID });
+
+                entity.Property(e => e.RoleID)
+                .HasColumnName("SYSUserRoleID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.UserID)
+                .HasColumnName("SYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.LookUpRoleID)
+                .HasColumnName("LOOKUPRoleID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.IsActive)
+                .HasColumnName("IsActive")
+                .HasColumnType("bit");
+
+                entity.Property(e => e.CreatedBy)
+                .HasColumnName("RowCreatedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.CreatedDateTime)
+                .HasColumnName("RowCreatedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.ModifiedBy)
+                .HasColumnName("RowModifiedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.ModifiedDateTime)
+                .HasColumnName("RowModifiedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.ToTable("LOOKUPRole");
+
+                entity.Property(e => e.RoleID)
+                .HasColumnName("LOOKUPRoleID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.RoleName)
+                .HasColumnName("RoleName")
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                entity.Property(e => e.RoleDescription)
+                .HasColumnName("RoleDescription")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+                entity.Property(e => e.CreatedBy)
+                .HasColumnName("RowCreatedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.CreatedDateTime)
+                .HasColumnName("RowCreatedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.ModifiedBy)
+                .HasColumnName("RowModifiedSYSUserID")
+                .HasColumnType("int");
+
+                entity.Property(e => e.ModifiedDateTime)
+                .HasColumnName("RowModifiedDateTime")
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            });
+        }
+    }
+}
+
---
+++ ENTJAVA_Week8/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ENTJAVA_Week8.Models.DB;
+using ENTJAVA_Week8.Models.EntityManager;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+
+namespace ENTJAVA_Week8.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        private readonly string[] userAssignedRoles;
+
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool authorize = false;
+
+            using (MyDBContext db = new MyDBContext())
+            {
+                UserManager um = new UserManager();
+                foreach (var role in userAssignedRoles)
+                {
+                    authorize = um.IsUserInRole(context.HttpContext.User.Identity.Name, role);
+                    if (authorize)
+                        return;
+                }
+            }
+
+            context.Result = new RedirectResult("~/Home/UnAuthorized"); // Need to create a separate page
+        }
+    }
+}
